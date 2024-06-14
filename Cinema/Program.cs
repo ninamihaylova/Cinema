44d70d781cbfedcc87e53a89819c6a3c2362286b
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 22:52:15 +0300

    Initial commit

---
+++ Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Tracing;
+
+class Cinema
+{
+
+    public static void Main(string[] args)
+    {
+        string typeMovie = Console.ReadLine();
+        int countRows = int.Parse(Console.ReadLine());
+        int seatsRow = int.Parse(Console.ReadLine());
+
+        double premier = 12.00;
+        double normal = 7.50;
+        double discount = 5.00;
+        double price = 0.00;
+
+        if (typeMovie == "Premiere")
+        {
+            price = ( countRows * seatsRow)*premier;
+            Console.WriteLine($"{price:F2}");
+        }
+        else if(typeMovie=="Normal")
+        {
+            price = (countRows * seatsRow) * normal;
+            Console.WriteLine($"{price:F2}");
+        }
+        else if (typeMovie=="Discount")
+        {
+            price = (countRows * seatsRow) * discount;
+            Console.WriteLine($"{price:F2}");
+
+        }
+    }
+}
